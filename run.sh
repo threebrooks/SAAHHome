cf96feb7287c352f6096f440201788479e4181ad
Ralf Meermeier <ralf.meermeier@gmail.com>
2020-09-20 03:40:49 +0100

    Getting there

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-armhf
 
-. install/Godec/env.sh
-. install/env.sh
+SAAH_INSTALL=~/SAAH/install
+export PYTHONPATH=$SAAH_INSTALL
+. $SAAH_INSTALL/Godec/env.sh
+. $SAAH_INSTALL/env.sh
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/boost_1_74_0/stage/lib
 
