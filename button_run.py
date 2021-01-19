80b488a8051f9b62a4478093c032732619154891
Ralf Meermeier <ralf.meermeier@gmail.com>
2021-01-19 04:21:56 +0000

    Bla

--- button_run.py
+++ button_run.py
@@ -7,6 +7,25 @@ import time
 import datetime
 import RPi.GPIO as GPIO
 
+def convertColor(hexCode):
+    R = int(hexCode[:2],16)
+    G = int(hexCode[2:4],16)
+    B = int(hexCode[4:6],16)
+
+    total = R + G + B
+
+    if R == 0:
+        firstPos = 0
+    else:
+        firstPos = R / total
+    
+    if G == 0:
+        secondPos = 0
+    else:
+        secondPos = G / total
+
+    return [firstPos, secondPos]
+
 ikealicht = SmartPlug("192.168.86.155")
 biglight = Bridge('192.168.86.26')
 biglight.connect()
@@ -18,10 +37,11 @@ async def Ikealicht(onoff):
   else:
     await ikealicht.turn_off()
 
-async def Biglight(onoff, brightness):
+async def Biglight(onoff, brightness, color):
   if (onoff):
     biglight.set_light(4, 'on', True)
     biglight.set_light(4, 'bri', brightness)
+    biglight.set_light(4, 'xy', convertColor(color))
   else:
     biglight.set_light(4, 'on', False)
  
@@ -36,9 +56,9 @@ while(True):
     now = datetime.datetime.now()
     if (now.hour > 21 or now.hour < 9):
       asyncio.run(Ikealicht(False))
-      asyncio.run(Biglight(onoff, 16))
+      asyncio.run(Biglight(onoff, 16, 'AB2424'))
     else:
       asyncio.run(Ikealicht(onoff))
-      asyncio.run(Biglight(onoff, 254))
+      asyncio.run(Biglight(onoff, 254, 'FFFFFF'))
     print("Onoff: "+str(onoff))
     time.sleep(1)
