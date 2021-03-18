b2ef90eae618507c03f5d404da9daba46e2d77be
Ralf Meermeier <ralf.meermeier@gmail.com>
2021-03-18 18:43:21 -0400

    Bla

--- button_run.py
+++ button_run.py
@@ -1,5 +1,6 @@
 import asyncio
 import sys
+import os
 import numpy as np
 from kasa import SmartPlug
 from phue import Bridge
@@ -48,20 +49,31 @@ async def Biglight(onoff, brightness, color):
   else:
     biglight.set_light(light_name, 'on', False)
  
-onoff = True
+lights_onoff = False
 button_pin = 9
+prev_button = 0
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN, GPIO.PUD_DOWN)
 while(True):
   button = GPIO.input(button_pin)
-  if (button == 1):
-    onoff = not onoff
-    now = datetime.datetime.now()
-    if (now.hour >= 21 or now.hour < 7):
-      asyncio.run(Ikealicht(False))
-      asyncio.run(Biglight(onoff, 16, 'AB2424'))
+  if (button == 1 and prev_button == 0): # Down
+    btn_down_time = time.time()
+  elif (button == 0 and prev_button == 1): # Up
+    if (time.time()-btn_down_time < 2.0):    
+      lights_onoff = not lights_onoff
+      now = datetime.datetime.now()
+      if (now.hour >= 21 or now.hour < 7):
+        asyncio.run(Ikealicht(lights_onoff))
+        asyncio.run(Biglight(lights_onoff, 16, 'AB2424'))
+      else:
+        asyncio.run(Ikealicht(lights_onoff))
+        asyncio.run(Biglight(lights_onoff, 254, 'FFFFFF'))
+      print("Onoff: "+str(lights_onoff))
     else:
-      asyncio.run(Ikealicht(onoff))
-      asyncio.run(Biglight(onoff, 254, 'FFFFFF'))
-    print("Onoff: "+str(onoff))
-    time.sleep(1)
+      print("Projector")
+      os.system("irsend send_start soundbar KEY_POWER; sleep 1; irsend send_stop soundbar KEY_POWER")
+      time.sleep(1)
+      print("Sound bar")
+      os.system("irsend send_start projector KEY_POWER; sleep 1; irsend send_stop projector KEY_POWER")
+  time.sleep(0.1)
+  prev_button = button
--- install_button_service.sh
+++ install_button_service.sh
@@ -20,3 +20,12 @@ WantedBy=multi-user.target" > /lib/systemd/system/ButtonLight.service
 sudo systemctl enable ButtonLight.service
 sudo systemctl start ButtonLight.service
 
+#sudo apt-get install lirc
+# Add to /boot/config.txt:
+#dtoverlay=gpio-ir-tx,gpio_pin=17
+#dtoverlay=gpio-ir,gpio_pin=18
+# Change /etc/lirc/lirc_options.conf:
+# driver=default
+# Copy *.conf to /etc/lirc/lircd.conf.d/
+
+
